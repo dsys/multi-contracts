c33150619e41f050a0e3739318274ea82f1be86a
Alex Kern <alex@kern.io>
2018-05-18 13:56:30 -0700

    WIP: Comment out new claim registry arch temp

--- contracts/ClaimRegistry.sol
+++ contracts/ClaimRegistry.sol
@@ -1,44 +1,44 @@
-pragma solidity ^0.4.23;
-
-import "./Ownable.sol";
-
-contract ClaimRegistry is Ownable {
-
-    struct Claim {
-        address issuer;
-        address subject;
-        bytes32 key;
-        bytes value;
-        uint256 nonce;
-        uint256 expires;
-        uint8 sigV;
-        bytes32 sigR;
-        bytes32 sigS;
-    }
-
-    mapping(address => mapping(bytes32 => Claim)) public registry;
-    mapping(address => mapping(bytes32 => bool)) public presence;
-
-    event ClaimSet(address indexed _subject, bytes32 indexed _key, bytes _val, uint _setAt);
-    event ClaimRemoved(address indexed _subject, bytes32 indexed _key, uint _setAt);
-
-    function hasClaim(address _subject, bytes32 _key) external view returns (bool) {
-        return presence[_subject][_key];
-    }
-
-    function getClaim(address _subject, bytes32 _key) external view returns (Claim) {
-        return registry[_subject][_key];
-    }
-
-    function setClaim(address _subject, bytes32 _key, bytes _val) onlyOwner external {
-        registry[_subject][_key] = _val;
-        presence[_subject][_key] = true;
-        emit ClaimSet(_subject, _key, _val, block.timestamp);
-    }
-
-    function removeClaim(address _subject, bytes32 _key) onlyOwner external {
-        presence[_subject][_key] = false;
-        emit ClaimRemoved(_subject, _key, block.timestamp);
-    }
-
-}
+// pragma solidity ^0.4.23;
+// 
+// import "./Ownable.sol";
+// 
+// contract ClaimRegistry is Ownable {
+// 
+//     struct Claim {
+//         address issuer;
+//         address subject;
+//         bytes32 key;
+//         bytes value;
+//         uint256 nonce;
+//         uint256 expires;
+//         uint8 sigV;
+//         bytes32 sigR;
+//         bytes32 sigS;
+//     }
+// 
+//     mapping(address => mapping(bytes32 => Claim)) public registry;
+//     mapping(address => mapping(bytes32 => bool)) public presence;
+// 
+//     event ClaimSet(address indexed _subject, bytes32 indexed _key, bytes _val, uint _setAt);
+//     event ClaimRemoved(address indexed _subject, bytes32 indexed _key, uint _setAt);
+// 
+//     function hasClaim(address _subject, bytes32 _key) external view returns (bool) {
+//         return presence[_subject][_key];
+//     }
+// 
+//     function getClaim(address _subject, bytes32 _key) external view returns (Claim) {
+//         return registry[_subject][_key];
+//     }
+// 
+//     function setClaim(address _subject, bytes32 _key, bytes _val) onlyOwner external {
+//         registry[_subject][_key] = _val;
+//         presence[_subject][_key] = true;
+//         emit ClaimSet(_subject, _key, _val, block.timestamp);
+//     }
+// 
+//     function removeClaim(address _subject, bytes32 _key) onlyOwner external {
+//         presence[_subject][_key] = false;
+//         emit ClaimRemoved(_subject, _key, block.timestamp);
+//     }
+// 
+// }
--- contracts/IdentityRegistry.sol
+++ contracts/IdentityRegistry.sol
@@ -1,100 +1,98 @@
-pragma solidity ^0.4.23;
-
-import "./ClaimRegistry.sol";
-import "./Ownable.sol";
-
-contract IdentityRegistry {
-
-    mapping(address => ClaimRegistry) public claimRegistries;
-    mapping(address => uint) public nonces;
-
-    modifier onlyOwner(address _subject) {
-        require(msg.sender == _subject);
-        _;
-    }
-
-    function getClaimRegistry() external view returns (ClaimRegistry) {
-        return claimRegistries[msg.sender];
-    }
-
-    function getClaimRegistry(address _subject) external view returns (ClaimRegistry) {
-        return claimRegistries[_subject];
-    }
-
-    function setClaimRegistry(ClaimRegistry claimRegistry) external {
-        claimRegistries[msg.sender] = claimRegistry;
-    }
-
-    function setClaimRegistry(address _subject, ClaimRegistry claims, uint8 sigV, bytes32 sigR, bytes32 sigS) external {
-        bytes32 prefixedHash = keccak256("\x19Ethereum Signed Message:\n32", _subject, claims, nonces[_subject]);
-        address signer = ecrecover(prefixedHash, sigV, sigR, sigS);
-        require(signer == _subject);
-        claimRegistries[_subject] = claims;
-        nonces[_subject]++;
-        return signer;
-    }
-
-    function hasClaim(address _subject, bytes32 _key) external view returns (bool) {
-        ClaimRegistry claims = claimRegistries[_subject];
-        if (address(claims) == 0) return 0;
-        return claims.hasClaim(0, _key);
-    }
-
-    function hasClaim(address _issuer, address _subject, bytes32 _key) external view returns (bool) {
-        ClaimRegistry claims = claimRegistries[_subject];
-        if (address(claims) == 0) return false;
-        return true;
-    }
-
-    function hasClaim(address[] _issuers, address _subject, bytes32 _key) external view returns (bool) {
-        for (uint i = 0; i < _issuers.length; i++) {
-            address issuer = _issuers[i];
-            ClaimRegistry claims = claimRegistries[issuer];
-            if (claims.hasClaim(_subject, _key)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    function getClaim(address _subject, bytes32 _key) external returns (ClaimRegistry.Claim) {
-        ClaimRegistry claims = claimRegistries[_subject];
-        if (address(claims) == 0) return 0;
-        return claims.getClaim(0, _key);
-    }
-
-    function getClaim(address _issuer, address _subject, bytes32 _key) external pure returns (ClaimRegistry.Claim) {
-        ClaimRegistry claims = claimRegistries[_issuer];
-        if (address(claims) == 0) return 0;
-        return claims.getClaim(_issuer, _subject, _key);
-    }
-
-    function getClaim(address[] _issuers, address _subject, bytes32 _key) external pure returns (ClaimRegistry.Claim) {
-        for (uint i = 0; i < _issuers.length; i++) {
-            address issuer = _issuers[i];
-            ClaimRegistry claims = claimRegistries[issuer];
-            if (claims.hasClaim(_subject, _key)) {
-                return claims.getClaim(issuer, _key);
-            }
-        }
-        return ClaimRegistry.Claim(0);
-    }
-
-    function verifyClaim(address _issuer, address _subject, bytes32 _key) external pure returns (ClaimRegistry.Claim) {
-        ClaimRegistry.Claim memory claim = this.getClaim(_issuer, _subject, _key);
-        require(address(claim) != 0 && checkClaimStruct(claim, _subject));
-        return claim;
-    }
-
-    function verifyClaim(address[] _issuers, address _subject, bytes32 _key) external pure returns (ClaimRegistry.Claim) {
-        ClaimRegistry.Claim memory claim = this.getClaim(_issuers, _subject, _key);
-        require(address(claim) != 0 && checkClaimStruct(claim, _subject));
-        return claim;
-    }
-
-    function checkClaimStruct(ClaimRegistry.Claim _claim, address _subject) internal pure returns (bool) {
-        bytes32 prefixedHash = keccak256("\x19Ethereum Signed Message:\n32", _claim.issuer, _claim.key, _claim.value, _claim.nonce, _claim.expires);
-        return ecrecover(prefixedHash, _claim.sigV, _claim.sigR, _claim.sigS) == _subject ? _claim : ClaimRegistry.Claim(0);
-    }
-
-}
+// pragma solidity ^0.4.23;
+// 
+// import "./ClaimRegistry.sol";
+// import "./Ownable.sol";
+// 
+// contract IdentityRegistry {
+// 
+//     mapping(address => ClaimRegistry) public claimRegistries;
+//     mapping(address => uint) public nonces;
+// 
+//     modifier onlyOwner(address _subject) {
+//         require(msg.sender == _subject);
+//         _;
+//     }
+// 
+//     function getClaimRegistry() external view returns (ClaimRegistry) {
+//         return claimRegistries[msg.sender];
+//     }
+// 
+//     function getClaimRegistry(address _subject) external view returns (ClaimRegistry) {
+//         return claimRegistries[_subject];
+//     }
+// 
+//     function setClaimRegistry(ClaimRegistry claimRegistry) external {
+//         claimRegistries[msg.sender] = claimRegistry;
+//     }
+// 
+//     function setClaimRegistry(address _subject, ClaimRegistry claims, uint8 sigV, bytes32 sigR, bytes32 sigS) external {
+//         bytes32 prefixedHash = keccak256("\x19Ethereum Signed Message:\n32", _subject, claims, nonces[_subject]);
+//         address signer = ecrecover(prefixedHash, sigV, sigR, sigS);
+//         require(signer == _subject);
+//         claimRegistries[_subject] = claims;
+//         nonces[_subject]++;
+//     }
+// 
+//     function hasClaim(address _subject, bytes32 _key) external view returns (bool) {
+//         ClaimRegistry claims = claimRegistries[_subject];
+//         return address(claims) != 0 && claims.hasClaim(0, _key);
+//     }
+// 
+//     function hasClaim(address _issuer, address _subject, bytes32 _key) external view returns (bool) {
+//         ClaimRegistry claims = claimRegistries[_subject];
+//         if (address(claims) == 0) return false;
+//         return true;
+//     }
+// 
+//     function hasClaim(address[] _issuers, address _subject, bytes32 _key) external view returns (bool) {
+//         for (uint i = 0; i < _issuers.length; i++) {
+//             address issuer = _issuers[i];
+//             ClaimRegistry claims = claimRegistries[issuer];
+//             if (claims.hasClaim(_subject, _key)) {
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// 
+//     function getClaim(address _subject, bytes32 _key) external returns (ClaimRegistry.Claim) {
+//         ClaimRegistry claims = claimRegistries[_subject];
+//         require(address(claims) != 0);
+//         return claims.getClaim(0, _key);
+//     }
+// 
+//     function getClaim(address _issuer, address _subject, bytes32 _key) external pure returns (ClaimRegistry.Claim) {
+//         ClaimRegistry claims = claimRegistries[_issuer];
+//         require(address(claims) != 0);
+//         return claims.getClaim(_issuer, _subject, _key);
+//     }
+// 
+//     function getClaim(address[] _issuers, address _subject, bytes32 _key) external pure returns (ClaimRegistry.Claim) {
+//         for (uint i = 0; i < _issuers.length; i++) {
+//             address issuer = _issuers[i];
+//             ClaimRegistry claims = claimRegistries[issuer];
+//             if (claims.hasClaim(_subject, _key)) {
+//                 return claims.getClaim(issuer, _key);
+//             }
+//         }
+//         return ClaimRegistry.Claim(0);
+//     }
+// 
+//     function verifyClaim(address _issuer, address _subject, bytes32 _key) external pure returns (ClaimRegistry.Claim) {
+//         ClaimRegistry.Claim storage claim = this.getClaim(_issuer, _subject, _key);
+//         require(address(claim) != 0 && checkClaimSignature(claim, _subject));
+//         return claim;
+//     }
+// 
+//     function verifyClaim(address[] _issuers, address _subject, bytes32 _key) external pure returns (ClaimRegistry.Claim) {
+//         ClaimRegistry.Claim storage claim = this.getClaim(_issuers, _subject, _key);
+//         require(address(claim) != 0 && checkClaimSignature(claim, _subject));
+//         return claim;
+//     }
+// 
+//     function checkClaimSignature(ClaimRegistry.Claim _claim, address _subject) internal pure returns (bool) {
+//         bytes32 prefixedHash = keccak256("\x19Ethereum Signed Message:\n32", _claim.issuer, _claim.key, _claim.value, _claim.nonce, _claim.expires);
+//         return ecrecover(prefixedHash, _claim.sigV, _claim.sigR, _claim.sigS) == _subject;
+//     }
+// 
+// }
