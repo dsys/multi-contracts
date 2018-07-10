f784b6c4a1d7e196bb8fa0dc609b246d47baee7b
Alex Kern <alex@kern.io>
2018-07-09 19:58:24 -0700

    Add SignatureUtils dependency

--- contracts/Identity.sol
+++ contracts/Identity.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.4.24;
 
 import { KeyManagement } from "./KeyManagement.sol";
+import { SignatureUtils } from "solidity-sigutils/contracts/SignatureUtils.sol";
 
 /// @title KeyManagement
 /// @author Alex Kern <alex@distributedsystems.com>
@@ -18,6 +19,7 @@ contract Identity {
     uint256 public constant OPERATION_DELEGATECALL = 1;
     uint256 public constant OPERATION_CREATE = 2;
 
+    using SignatureUtils for bytes32;
     using KeyManagement for KeyManagement.KeyManager;
 
     event KeyAdded(bytes32 indexed key, uint256 indexed purpose, uint256 indexed keyType);
@@ -145,7 +147,15 @@ contract Identity {
         bytes32 _messageHash,
         bytes _messageSignatures
     ) public {
-        require(true == true); // TODO
+        address[] memory addresses = _messageHash.recoverAddresses(_messageSignatures);
+        for (uint256 i = 0; i < addresses.length; i++) {
+            bytes32 keyId = bytes32(addresses[i]);
+            if (manager.keyHasPurpose(keyId, MANAGEMENT_KEY)) {
+                return;
+            }
+        }
+
+        revert("No valid signatures provided");
     }
 
     /// @dev Returns the message hash that must be signed to execute a transaction
@@ -229,26 +239,15 @@ contract Identity {
         );
 
         verifyMessageHash(
-            messageHash,
+            messageHash.toEthBytes32SignedMessageHash(),
             _messageSignatures
         );
 
         nonce++; // increment nonce to prevent reentrancy
 
-        _executeCall(_to, _value, _data);
+        success = _executeCall(_to, _value, _data);
 
-        // TODO: Implement ERC 1077.
-    }
-
-    function _executeCall(
-        address _to,
-        uint256 _value,
-        bytes _data
-    ) internal returns (bool success) {
-        // solium-disable-next-line security/no-inline-assembly
-        assembly {
-            success := call(gas, _to, _value, add(_data, 0x20), mload(_data), 0, 0)
-        }
+        // TODO: Continue implementing ERC 1077.
     }
 
     /// @dev Estimates the amount of gas used by the transaction
@@ -284,233 +283,19 @@ contract Identity {
         // TODO: Implement ERC 1077.
     }
 
-    // function execute(address _to, uint256 _value, bytes _data) external returns (bool) {
-    //     if ((_to == address(this) && !isManagementAddress) && !isActionAddress(msg.sender)) return false;
-    //     return _executeCall(_to, _value, _data);
-    // }
-
-    // function executeCallSigned(address _to, uint256 _value, bytes _data, bytes _sig) external returns (bool) {
-    //     require(_to != address(this) && _to != address(0));
-    //     bytes32 message = getExecuteCallSignedMessage(_to, _value, _data);
-    //     require(meetsSignerThreshold(message, _sig));
-    //     return _executeCall(_to, _value, _data);
-    // }
-
-    // function _executeCall(address _to, uint256 _value, bytes _data) internal returns (bool success) {
-    //     require(_to != address(this) && _to != address(0));
-
-    //     // increment nonce to prevent reentrancy
-    //     nonce++;
-
-    //     // solium-disable-next-line security/no-inline-assembly
-    //     assembly {
-    //         success := call(gas, _to, _value, add(_data, 0x20), mload(_data), 0, 0)
-    //     }
-
-    //     emit CallExecuted(_to, _value, _data, block.timestamp); // solium-disable-line security/no-block-members
-    // }
-
-    // Signer management
-    // ===========================================================================
-
-    // function isSignerSignature(bytes32 message, bytes sig) public view returns (bool) {
-    //     bytes32 hash = ECRecovery.toEthSignedMessageHash(message);
-    //     return isSigner(ECRecovery.recover(hash, sig));
-    // }
-
-    // function addSigner(address _address) onlyOwner external {
-    //     _addSigner(_address);
-    // }
-
-    // function addSignerSigned(address _address, bytes sig) external {
-    //     bytes32 message = getAddOwnerSignedMessage(_address);
-    //     require(isOwnerSignature(message, sig));
-    //     _addSigner(_address);
-    // }
-
-    // function getAddSignerSignedMessage(address _address) public view returns (bytes32) {
-    //     return keccak256(byte(0x19), byte(0), this, nonce, "addSigner", _address);
-    // }
-
-    // function _addSigner(address _address) internal {
-    //     if (isOwner(_address) || isSigner(_address)) return;
-    //     signersMapping[_address] = true;
-    //     signers.push(_address);
-    //     emit SignerAdded(_address, block.timestamp); // solium-disable-line security/no-block-members
-    // }
-
-    // function removeSigner(address _address) onlyOwner external {
-    //     _removeSigner(_address);
-    // }
-
-    // function removeSignerSigned(address _address, bytes sig) external {
-    //     bytes32 message = getRemoveSignerSignedMessage(_address);
-    //     require(isOwnerSignature(message, sig));
-    //     _removeSigner(_address);
-    // }
-
-    // function getRemoveSignerSignedMessage(address _address) public view returns (bytes32) {
-    //     return keccak256(byte(0x19), byte(0), this, nonce, "removeSigner", _address);
-    // }
-
-    // function _removeSigner(address _address) internal {
-    //     if (isOwner(_address) || !isSigner(_address)) return;
-    //     for (uint8 i = 0; i < signers.length; i++) {
-    //         if (_address == signers[i]) {
-    //             // replace the hole with the last element
-    //             if (i != i - 1) {
-    //                 signers[i] = signers[signers.length - 1];
-    //             }
-    //             delete signers[signers.length - 1];
-    //             signers.length--;
-    //             delete signersMapping[_address];
-
-    //             emit SignerRemoved(_address, block.timestamp); // solium-disable-line security/no-block-members
-    //             return;
-    //         }
-    //     }
-    // }
-
-    // // Threshold configuration
-    // // ===========================================================================
-
-    // function getSignerThreshold() external view returns (uint8) {
-    //     return signerThreshold;
-    // }
-
-    // function setSignerThreshold(uint8 _signerThreshold) onlyOwner external {
-    //     _setSignerThreshold(_signerThreshold);
-    // }
-
-    // function setSignerThresholdSigned(uint8 _signerThreshold, bytes sig) external {
-    //     bytes32 message = getSetSignerThresholdSignedMessage(_signerThreshold);
-    //     require(isOwnerSignature(message, sig));
-    //     _setSignerThreshold(_signerThreshold);
-    // }
-
-    // function _setSignerThreshold(uint8 _signerThreshold) internal {
-    //     signerThreshold = _signerThreshold;
-    //     emit SignerThresholdChanged(_signerThreshold, block.timestamp); // solium-disable-line security/no-block-members
-    // }
-
-    // function getSetSignerThresholdSignedMessage(uint8 _signerThreshold) public view returns (bytes32) {
-    //     return keccak256(byte(0x19), byte(0), this, nonce, "setSignerThreshold", _signerThreshold);
-    // }
-
-    // function meetsSignerThreshold(bytes32 _message, bytes _sig) public view returns (bool) {
-    //     if (_sig.length == 65) {
-    //         return isOwnerSignature(_message, _sig);
-    //     }
-
-    //     require(_sig.length % SIGNATURE_LENGTH == 0);
-
-    //     bytes32 hash = ECRecovery.toEthSignedMessageHash(_message);
-    //     uint signatureCount = _sig.length / SIGNATURE_LENGTH;
-
-    //     address[] memory signersReceived = new address[](signatureCount);
-    //     uint8 uniqueCount;
-
-    //     for (uint i = 0; i < signatureCount; i++) {
-    //         address addr = recoverKey(hash, _sig, i);
-    //         if (ownersMapping[addr]) {
-    //             // if an owner signature is present, accept
-    //             return true;
-    //         } else if (signersMapping[addr]) {
-    //             // only count unique signer signatures
-    //             bool found = false;
-    //             for (uint8 j = 0; j < uniqueCount; j++) {
-    //                 if (addr == signersReceived[j]) {
-    //                     found = true;
-    //                     break;
-    //                 }
-    //             }
-    //             if (!found) {
-    //                 signersReceived[uniqueCount] = addr;
-    //                 uniqueCount++;
-    //             }
-    //         }
-    //     }
-
-    //     return uniqueCount >= 1 && uniqueCount >= signerThreshold;
-    // }
-
-    // // Execute
-    // // ===========================================================================
-
-    // function executeCall(address _to, uint256 _value, bytes _data) external returns (bool) {
-    //     require(isOwner(msg.sender) || (isSigner(msg.sender) && signerThreshold <= 1));
-    //     return _executeCall(_to, _value, _data);
-    // }
-
-    // // TODO: Might be able to process the signatures as a single bytes array if they're a fixed length.
-    // function executeCallSigned(address _to, uint256 _value, bytes _data, bytes _sig) external returns (bool) {
-    //     require(_to != address(this) && _to != address(0));
-    //     bytes32 message = getExecuteCallSignedMessage(_to, _value, _data);
-    //     require(meetsSignerThreshold(message, _sig));
-    //     return _executeCall(_to, _value, _data);
-    // }
-
-    // function _executeCall(address _to, uint256 _value, bytes _data) internal returns (bool success) {
-    //     require(_to != address(this) && _to != address(0));
-
-    //     // increment nonce to prevent reentrancy
-    //     nonce++;
-
-    //     // solium-disable-next-line security/no-inline-assembly
-    //     assembly {
-    //         success := call(gas, _to, _value, add(_data, 0x20), mload(_data), 0, 0)
-    //     }
-
-    //     emit CallExecuted(_to, _value, _data, block.timestamp); // solium-disable-line security/no-block-members
-    // }
-
-    // function getExecuteCallSignedMessage(address _to, uint256 _value, bytes _data) public view returns (bytes32) {
-    //     return keccak256(byte(0x19), byte(0), this, nonce, "executeCall", _to, _value, _data);
-    // }
-
-    // // TODO: Extract signature utils into npm module.
-
-    // function recoverKey (
-    //     bytes32 _hash, 
-    //     bytes _sigs,
-    //     uint256 _pos
-    // ) private pure returns (address) {
-    //     uint8 v;
-    //     bytes32 r;
-    //     bytes32 s;
-    //     (v, r, s) = signatureSplit(_sigs, _pos);
-    //     return ecrecover(
-    //         _hash,
-    //         v,
-    //         r,
-    //         s
-    //     );
-    // }
-
-    // function signatureSplit(
-    //     bytes _signatures,
-    //     uint256 _pos
-    // ) private pure returns (uint8 v, bytes32 r, bytes32 s) {
-    //     uint256 offset = _pos * SIGNATURE_LENGTH;
-
-    //     // solium-disable-next-line security/no-inline-assembly
-    //     assembly {
-    //         r := mload(add(_signatures, add(32, offset)))
-    //         s := mload(add(_signatures, add(64, offset)))
-    //         // Here we are loading the last 32 bytes, including 31 bytes
-    //         // of 's'. There is no 'mload8' to do this.
-    //         //
-    //         // 'byte' is not working due to the Solidity parser, so lets
-    //         // use the second best option, 'and'
-    //         v := and(mload(add(_signatures, add(65, offset))), 0xff)
-    //     }
-
-    //     // Version of signature should be 27 or 28, but 0 and 1 are also possible versions
-    //     if (v < 27) {
-    //         v += 27;
-    //     }
-
-    //     require(v == 27 || v == 28);
-    // }
+    /// @dev Executes a call to another contract
+    /// @param _to The contract to call
+    /// @param _value The value to attach to the call
+    /// @param _data The data for the call
+    function _executeCall(
+        address _to,
+        uint256 _value,
+        bytes _data
+    ) internal returns (bool success) {
+        // solium-disable-next-line security/no-inline-assembly
+        assembly {
+            success := call(gas, _to, _value, add(_data, 0x20), mload(_data), 0, 0)
+        }
+    }
 
 }
--- migrations/1525467622_create_identity_factory.js
+++ migrations/1525467622_create_identity_factory.js
@@ -1,7 +1,11 @@
+const SignatureUtils = artifacts.require("SignatureUtils");
 const KeyManagement = artifacts.require("KeyManagement");
+const Identity = artifacts.require("Identity");
 const IdentityFactory = artifacts.require("IdentityFactory");
 
 module.exports = async function(deployer) {
+  await deployer.deploy(SignatureUtils);
+  await deployer.link(SignatureUtils, [Identity, IdentityFactory]);
   await deployer.deploy(KeyManagement);
   await deployer.deploy(IdentityFactory);
 };
